3f887ef69118156a0f470417cd6f4f3317dc4af8
ra44o <ra44elo@gmail.com>
2019-10-30 16:43:22 +0300

    feat: add the file with implementetion of the task

---
+++ index.js
@@ -0,0 +1,28 @@
+// task-1
+
+// function that reverses a string
+function getReversedString(str) {
+  if (typeof str === 'string') {
+    return str.split('').reverse().join('');
+  }
+  return;
+}
+
+// function that replaces part of the string for substring
+function get(str, oldSubStr, newSubStr) {
+  if (typeof str === 'string' && typeof oldSubStr === 'string' && typeof newSubStr === 'string') {
+    return str.replace(oldSubStr, newSubStr);
+  }
+  return;
+}
+
+// function that returns values of a given object and nested objects in it 
+function getValuesOfObject(obj) {
+  for (let value in obj) {
+    if (typeof obj[value] === 'object') {
+      getValuesOfObject(obj[value]);
+    } else {
+      console.log(obj[value]);
+    }
+  }
+}
