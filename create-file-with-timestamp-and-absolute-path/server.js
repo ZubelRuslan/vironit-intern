de54fc0762cd20d7b256a402bf8958e4318ce064
ra44o <ra44elo@gmail.com>
2019-11-04 23:28:19 +0300

    fix: rename folder and file

--- create-file-with-timestamp-and-absolute-path/server.js
+++ create-file-with-timestamp-and-absolute-path/server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const fs = require('fs');
 
 fs.writeFile(
-    'kek.txt',
-    `${new Date().getTime()}\n${path.join(__dirname, 'kek.txt')}`,
+    'output.txt',
+    `${new Date().getTime()}\n${path.join(__dirname, 'output.txt')}`,
     err => {
         if (err) throw new Error(err);
     }
