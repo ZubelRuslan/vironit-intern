321a5147e03790db4d4890b5653e2635c5adca03
ra44o <ra44elo@gmail.com>
2019-11-11 15:13:24 +0300

    fix: add endings to responses

--- express-server/index.js
+++ express-server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const helmet = require('helmet');
 
 const app = express();
 const PORT = 5000;
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,10 +18,12 @@ app.get('/:filename', (req, res) => {
         res.status = 400;
         res.setHeader('Content-Type', 'text/html');
         res.send('<h1>No such file</h1>');
+        res.end();
       } else {
         res.status = 200;
         res.setHeader('Content-Type', 'text/html');
         res.send(`<h1>Data in the file:</h1><p>${data}</p>`);
+        res.end();
       }
     }
   )
@@ -41,10 +45,12 @@ app.post('/:filename', (req, res) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
         res.send(`<h1>File ${req.params.filename.toUpperCase()} created</h1>`);
+        res.end();
       } else {
         res.statusCode = 400;
         res.setHeader('Content-Type', 'text/html');
         res.send(`<h1>File with name ${req.params.filename.toUpperCase()} already exist</h1>`);
+        res.end();
       }
     }
   );
@@ -59,11 +65,13 @@ app.put('/:filename', (req, res) => {
       res.statusCode = 400;
       res.setHeader('Content-Type', 'text/html');
       res.send(`<h1>File with name ${req.params.filename.toUpperCase()} already exist</h1>`);
+      res.end();
     }
   );
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.send(`<h1>File ${req.params.filename.toUpperCase()} updated</h1>`);
+  res.end();
 });
 
 app.delete('/:filename', (req, res) => {
@@ -73,11 +81,13 @@ app.delete('/:filename', (req, res) => {
       res.statusCode = 400;
       res.setHeader('Content-Type', 'text/html');
       res.send(`<h1>File with name ${req.params.filename.toUpperCase()} doesn't exist</h1>`);
+      res.end();
     }
   );
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.send(`<h1>File ${req.params.filename.toUpperCase()} deleted</h1>`);
+  res.end();
 });
 
 app.listen(PORT, () => {
